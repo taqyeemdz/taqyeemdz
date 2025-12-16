3bd9a56e4e164d09a46102509baffcd72f6ad932
yousra's projects <yousra.ayn@gmail.com>
2025-12-16 14:47:38 +0100

    taqyeem 8

--- lib/resend.ts
+++
@@ -1,9 +0,0 @@
-import { Resend } from "resend";
-
-const apiKey = process.env.RESEND_API_KEY;
-
-if (!apiKey) {
-    throw new Error("❌ RESEND_API_KEY is missing");
-}
-
-export const resend = new Resend(apiKey);
